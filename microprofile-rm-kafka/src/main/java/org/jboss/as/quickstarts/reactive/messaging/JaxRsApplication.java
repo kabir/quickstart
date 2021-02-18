4532e19109e2b47702fc8f7dad44e689cfa114fb
Kabir Khan <kkhan@redhat.com>
2021-02-18 10:46:24 +0000

    [WFLY-14391] MicroProfile Reactive Messaging with Kafka Quickstart

---
+++ microprofile-rm-kafka/src/main/java/org/jboss/as/quickstarts/reactive/messaging/JaxRsApplication.java
@@ -0,0 +1,8 @@
+package org.jboss.as.quickstarts.reactive.messaging;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JaxRsApplication extends Application {
+}
